d9248dbf16bf6752ea740657c27433df0e5e83dc
Ivan1994Petrov <ivan94hristov@gmail.com>
2019-08-09 12:36:24 +0300

    Adding Win/Loss Ratio to the view.py and home_page.html

--- bundesliga/views.py
+++ bundesliga/views.py
@@ -10,10 +10,11 @@ from .services import get_upcoming_matches
 
 def bundeslig_detail(request):
     return_from_all_upcoming_maches = get_upcoming_matches()
+
     SELECTORS = {
         'all-matches-teams': return_from_all_upcoming_maches['all_matches'].values(),
-        'all-upcoming-maches-teams': return_from_all_upcoming_maches['all_upcoming_maches'].values()
-
+        'all-upcoming-maches-teams': return_from_all_upcoming_maches['all_upcoming_maches'].values(),
+        'all_teams_ratio': return_from_all_upcoming_maches['all_teams_ratio']
     }
 
     def get_zipped_info_for_all_matches(arr):
@@ -38,8 +39,23 @@ def bundeslig_detail(request):
 
         return zip(all_matches_home_teams, all_matches_away_teams, all_matches_date)
 
-    print(list(get_zipped_info_for_all_upcoming_matches(SELECTORS['all-upcoming-maches-teams'])))
+    def get_zipped_info_for_win_loss(dict):
+        all_teams = []
+        all_wins = []
+        all_losses = []
+        all_drows = []
+        print(type(dict))
+        for key in dict.keys():
+            all_teams.append(key)
+            all_wins.append(dict[key].win)
+            all_losses.append(dict[key].loss)
+            all_drows.append(dict[key].draw)
+        return zip(all_teams, all_wins, all_losses, all_drows)
+
+    print(list(get_zipped_info_for_win_loss(return_from_all_upcoming_maches['all_teams_ratio'])))
+
     return render(request, 'home_page.html', {
         'all_teams': get_zipped_info_for_all_matches(SELECTORS['all-matches-teams']),
-        'all_upcoming_maches': get_zipped_info_for_all_upcoming_matches(SELECTORS['all-upcoming-maches-teams'])
+        'all_upcoming_maches': get_zipped_info_for_all_upcoming_matches(SELECTORS['all-upcoming-maches-teams']),
+        'all_teams_ratio': get_zipped_info_for_win_loss(SELECTORS['all_teams_ratio'])
     })
